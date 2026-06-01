d4718ed8d328006b9f66748410b97155f85bd1ca
XeroSWM <wxmonteros@uce.edu.ec>
2026-06-01 04:29:59 -0500

    Updata

--- fica-judge/apps/iam-service/src/main.ts
+++ fica-judge/apps/iam-service/src/main.ts
@@ -4,10 +4,11 @@ import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(IamServiceModule);
-  
+
   const PORT = process.env.IAM_PORT || 3001;
   await app.listen(PORT);
-  
+
   Logger.log(`🔐 [IAM Service] Escuchando en el puerto: ${PORT}`, 'Bootstrap');
 }
+
 bootstrap();
