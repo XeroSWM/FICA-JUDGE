42b9c6831727ade59838264c7af568edb09d507b
XeroSWM <wxmonteros@uce.edu.ec>
2026-06-08 13:24:34 -0500

    Update iam-service.module.ts

--- fica-judge/apps/iam-service/src/iam-service.module.ts
+++ fica-judge/apps/iam-service/src/iam-service.module.ts
@@ -3,10 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CqrsModule } from '@nestjs/cqrs';
 import { User } from './domain/entities/user.entity';
 import { IamController } from './infrastructure/controllers/iam.controller';
-import { HealthController } from './infrastructure/controllers/health.controller'; // IMPORTANTE: Importar el nuevo controlador
+import { HealthController } from './infrastructure/controllers/health.controller';
 import { RegisterUserHandler } from './application/commands/register-user.handler';
 
-// Agrupamos los handlers por si a futuro tenemos más (Login, Delete, etc.)
 const CommandHandlers = [RegisterUserHandler];
 
 @Module({
@@ -14,19 +13,19 @@ const CommandHandlers = [RegisterUserHandler];
     CqrsModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      // Leemos las variables inyectadas por Terraform en AWS. 
-      // Si no existen (ej. en tu PC), usa localhost por defecto.
       host: process.env.DB_HOST || 'localhost',
       port: parseInt(process.env.DB_PORT || '5432', 10),
       username: process.env.DB_USERNAME || 'iam_user',
       password: process.env.DB_PASSWORD || 'iam_password',
       database: process.env.DB_NAME || 'iam_db',
       entities: [User],
-      synchronize: true, // Mantenemos true para facilitar las pruebas en QA
+      synchronize: true,
+      // NUEVO: Habilitamos SSL si la variable DB_HOST existe (es decir, estamos en AWS)
+      ssl: process.env.DB_HOST ? { rejectUnauthorized: false } : false,
     }),
     TypeOrmModule.forFeature([User]),
   ],
-  controllers: [IamController, HealthController], // IMPORTANTE: Añadir el HealthController aquí
+  controllers: [IamController, HealthController],
   providers: [...CommandHandlers],
 })
 export class IamServiceModule {}
