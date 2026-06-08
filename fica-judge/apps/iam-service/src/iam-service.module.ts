fb574fa7dcfc4fcb8d95f5dafc02ab24a30a0eb7
XeroSWM <wxmonteros@uce.edu.ec>
2026-06-08 12:55:58 -0500

    update

--- fica-judge/apps/iam-service/src/iam-service.module.ts
+++ fica-judge/apps/iam-service/src/iam-service.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CqrsModule } from '@nestjs/cqrs';
 import { User } from './domain/entities/user.entity';
 import { IamController } from './infrastructure/controllers/iam.controller';
+import { HealthController } from './infrastructure/controllers/health.controller'; // IMPORTANTE: Importar el nuevo controlador
 import { RegisterUserHandler } from './application/commands/register-user.handler';
 
 // Agrupamos los handlers por si a futuro tenemos más (Login, Delete, etc.)
@@ -13,17 +14,19 @@ const CommandHandlers = [RegisterUserHandler];
     CqrsModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'iam_user',
-      password: 'iam_password',
-      database: 'iam_db',
+      // Leemos las variables inyectadas por Terraform en AWS. 
+      // Si no existen (ej. en tu PC), usa localhost por defecto.
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USERNAME || 'iam_user',
+      password: process.env.DB_PASSWORD || 'iam_password',
+      database: process.env.DB_NAME || 'iam_db',
       entities: [User],
-      synchronize: true,
+      synchronize: true, // Mantenemos true para facilitar las pruebas en QA
     }),
     TypeOrmModule.forFeature([User]),
   ],
-  controllers: [IamController],
+  controllers: [IamController, HealthController], // IMPORTANTE: Añadir el HealthController aquí
   providers: [...CommandHandlers],
 })
 export class IamServiceModule {}
--- fica-judge/apps/iam-service/src/main.ts
+++ fica-judge/apps/iam-service/src/main.ts
@@ -5,7 +5,6 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   // Creamos la aplicación NestJS
   const app = await NestFactory.create(IamServiceModule);
-  
   // 1. Habilitar CORS para permitir peticiones desde el S3 Frontend
   // Al dejarlo así, permites el origen de tu bucket S3 o cualquier otro que intente conectar
   app.enableCors({
@@ -20,10 +19,8 @@ async function bootstrap() {
 
   // 3. Configuración del puerto dinámico (para entornos Cloud)
   const PORT = process.env.IAM_PORT || 3001;
-  
   // 4. Iniciar el servicio
   await app.listen(PORT);
-  
   Logger.log(`🚀 [IAM Service] Escuchando en el puerto: ${PORT}`, 'Bootstrap');
   Logger.log(`🌍 [CORS] Habilitado globalmente`, 'Bootstrap');
 }
