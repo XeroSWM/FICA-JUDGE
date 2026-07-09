663341eb2976ce3f1a872865993241f0aa68cf74
XeroSWM <wxmonteros@uce.edu.ec>
2026-07-09 04:11:49 -0500

    new microservice

--- fica-judge/apps/assignment-service/src/main.ts
+++ fica-judge/apps/assignment-service/src/main.ts
@@ -4,11 +4,10 @@ import { AssignmentServiceModule } from './assignment-service.module';
 async function bootstrap() {
   const app = await NestFactory.create(AssignmentServiceModule);
   
-  // Habilitar CORS si es necesario comunicarse directo, o dejarlo solo para el Gateway
+  // Habilitamos CORS por si lo consultas directo en desarrollo
   app.enableCors();
   
-  // Asignamos el puerto 3005
   await app.listen(3005);
-  console.log(`📚 Assignment Service corriendo en: http://localhost:3005`);
+  console.log(`🚀 Assignment Service (Deberes y Exámenes) corriendo en http://localhost:3005`);
 }
 bootstrap();
