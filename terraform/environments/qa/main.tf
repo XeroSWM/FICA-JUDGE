d619792169e0ef7c2e3ff245b2fdb5ea5adf958a
XeroSWM <wxmonteros@uce.edu.ec>
2026-07-12 02:21:59 -0500

    update

--- terraform/environments/qa/main.tf
+++ terraform/environments/qa/main.tf
@@ -29,6 +29,13 @@ module "api_gateway" {
   app_port      = 3000
   docker_image  = "xxavyx38/api-gateway:latest"
   
+  # 👇 INYECCIÓN DINÁMICA DE RUTAS 👇
+  iam_service_url        = "http://${module.iam_service.service_url}/auth"
+  catalog_service_url    = "http://${module.catalog_service.service_url}/problems"
+  submission_service_url = "http://${module.submission_service.service_url}/submissions"
+  ranking_service_url    = "http://${module.ranking_service.service_url}/ranking"
+  assignment_service_url = "http://${module.assignment_service.service_url}/assignments"
+  
   # El Gateway solo enruta, no necesita BD propia
   requires_rds      = false
   requires_mongo    = false
